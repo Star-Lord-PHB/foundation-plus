6885f2192e471e3b3efa10d897c0eb3578b2bab6
Haobo Peng <fsphb@icloud.com>
2025-09-23 06:09:36 -0700

    feat: Add more property accessors for Date
    * Add `isInToday`, `isInTomorrow`, `isInYesterday`, `isInWeekend` and  `ordinality` methods

--- Sources/DatePlus/Date+QuickAccessor.swift
+++ Sources/DatePlus/Date+QuickAccessor.swift
@@ -53,6 +53,28 @@ extension Date {
     ) -> DateInterval {
         calendar.dateInterval(of: component.rawValue, for: self)!
     }
+
+
+    /// Returns the ordinality of a component within a larger component for the date.
+    /// 
+    /// - Parameter smaller: The component to find the ordinality of
+    /// - Parameter larger: The component to find the ordinality within
+    /// - Parameter calendar: The calendar for the calculation
+    /// 
+    /// - Returns: The ordinality of the component within the larger component for the date, or nil 
+    ///   if either the smaller component is not really smaller or the ordinality cannot be calculated
+    /// 
+    /// - Seealso: [`ordinality(of:in:for:)`]
+    /// 
+    /// [`ordinality(of:in:for:)`]: https://developer.apple.com/documentation/foundation/calendar/ordinality(of:in:for:)
+    @inlinable
+    public func ordinality(
+        of smaller: Calendar.Component, 
+        in larger: Calendar.Component, 
+        using calendar: Calendar = .current
+    ) -> Int? {
+        calendar.ordinality(of: smaller, in: larger, for: self)
+    }
     
     
     /// Computes the next date which matches (or most closely matches) a given set of components.
@@ -82,6 +104,46 @@ extension Date {
             repeatedTimePolicy: repeatedTimePolicy,
             direction: direction
         )
+    }
+
+
+    /// Return whether the date is within today
+    /// 
+    /// - Seealso: [`isDateInToday(_:)`]
+    /// 
+    /// [`isDateInToday(_:)`]: https://developer.apple.com/documentation/foundation/calendar/isdateintoday(_:)
+    public func isInToday(using calendar: Calendar = .current) -> Bool {
+        calendar.isDateInToday(self)
+    }
+
+
+    /// Return whether the date is within tomorrow
+    /// 
+    /// - Seealso: [`isDateInTomorrow(_:)`]
+    /// 
+    /// [`isDateInTomorrow(_:)`]: https://developer.apple.com/documentation/foundation/calendar/isdateintomorrow(_:)
+    public func isInTomorrow(using calendar: Calendar = .current) -> Bool {
+        calendar.isDateInTomorrow(self)
+    }
+
+
+    /// Return whether the date is within yesterday
+    /// 
+    /// - Seealso: [`isDateInYesterday(_:)`]
+    /// 
+    /// [`isDateInYesterday(_:)`]: https://developer.apple.com/documentation/foundation/calendar/isdateinyesterday(_:)
+    public func isInYesterday(using calendar: Calendar = .current) -> Bool {
+        calendar.isDateInYesterday(self)
+    }
+
+
+    /// Return whether the date is within a weekend
+    /// 
+    /// - Seealso: [`isDateInWeekend(_:)`]
+    /// 
+    /// [`isDateInWeekend(_:)`]: https://developer.apple.com/documentation/foundation/calendar/isdateinweekend(_:)
+    public func isInWeekend(using calendar: Calendar = .current) -> Bool {
+        calendar.isDateInWeekend(self)
     }
     
 }
